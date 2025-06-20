f7a75742ab58eadc5092e135ef7b6d4dc846fe10
Wemerson Damasceno <wemersondamasceno7@gmail.com>
2025-06-20 18:31:56 -0300

    feat: add new constants class - AppColors - AppImages

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color textColor = Color(0xFFF4F4F4);
+  static const Color infoColor = Colors.lightBlue;
+  static const Color white = Colors.white;
+  static const Color backgroundColor = Color(0xFFfabc6b);
+}
---
+++ lib/core/constants/app_images.dart
@@ -0,0 +1,3 @@
+class AppImages {
+  static const String header = 'assets/image_header.png';
+}
